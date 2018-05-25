b711640f537b0ef8cbd5635fe6d61fd28d84cbbc
mahmudin muttaqin <claxmanx@gmail.com>
2018-05-25 21:57:23 +0700

    add full width template

--- page-templates/full-width-page.php
+++ page-templates/full-width-page.php
@@ -1,26 +1,24 @@
-<?php get_header(); ?>
-
+<?php  
+/*
+Template Name: Full Width 
+*/
+get_header();
+?>
 	<?php if (have_posts()): ?>
 		
 		<div class="starter-template">
 
 			<div class="row">
 
-				<div class="col-sm-8">
+				<div class="col-sm-12">
 
 					<?php while (have_posts()): the_post(); ?>
 							
-							<?php get_template_part( 'template-parts/content', 'page' ); ?>				
+								<?php get_template_part( 'template-parts/content', 'page' ); ?>				
 
 					<?php endwhile ?>
 
 				</div>				
-
-				<div class="col-sm-4">
-					
-					<?php dynamic_sidebar('sidebar1'); ?>
-
-				</div>
 
 			</div>
 
